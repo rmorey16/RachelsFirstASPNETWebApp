355008e8b11f8d62832852290c062266a2718b8b
rmorey16 <moreyrachel@gmail.com>
2021-01-20 22:09:54 -0700

    Add project files.

---
+++ wwwroot/js/gradecalculator.js
@@ -0,0 +1,34 @@
+﻿var gradeSubmit = document.getElementById("gradeSubmit");
+var gradeNum = document.getElementById('gradeNum').value;
+var gradeLetter;
+
+if (gradeNum >= 94) {
+    gradeLetter = "A";
+} else if (gradeNum >= 90) {
+    gradeLetter = "A-";
+} else if (gradeNum >= 87) {
+    gradeLetter = "B+";
+} else if (gradeNum >= 84) {
+    gradeLetter = "B";
+} else if (gradeNum >= 80) {
+    gradeLetter = "B-";
+} else if (gradeNum >= 77) {
+    gradeLetter = "C+";
+} else if (gradeNum >= 74) {
+    gradeLetter = "C";
+} else if (gradeNum >= 70) {
+    gradeLetter = "C-";
+} else if (gradeNum >= 67) {
+    gradeLetter = "D+";
+} else if (gradeNum >= 64) {
+    gradeLetter = "D";
+} else if (gradeNum >= 60) {
+    gradeLetter = "D-";
+} else {
+    gradeLetter = "E";
+}
+gradeSubmit.addEventListener("click", function () {
+    alert("The alphabetical grade is: " + gradeLetter)
+});
+
+
